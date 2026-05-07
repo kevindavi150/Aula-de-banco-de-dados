1c655d50281aa89798c3ef7b2ee5e2713c30aa1e
Kevin <kevindavi150@gmail.com>
2026-05-07 08:47:53 -0300

    Add files via upload

---
+++ vendas/vendas.sql
@@ -0,0 +1,64 @@
+-- Linguagem SQL padrão ANSI-92 ou SQL-92
+-- Criação do banco de dados vendas
+CREATE DATABASE IF NOT EXISTS vendas;
+
+-- Definição do banco de dados padrão
+USE vendas;
+
+-- Criação da tabela de clientes
+CREATE TABLE IF NOT EXISTS clientes
+-- Campo CPF do tipo decimal que comporta 11 caracteres
+( cpf_cliente DECIMAL(11) PRIMARY KEY, -- campo único, não se repete
+  nome_cliente VARCHAR(80) NOT NULL,
+  sexo CHAR(1) NOT NULL,
+  data_nascimento DATE NOT NULL,
+  telefone VARCHAR(20), 
+  email VARCHAR(50),
+  localidade VARCHAR(60),
+  bairro VARCHAR(30),
+  estado CHAR(2),
+  pais VARCHAR(20)
+);
+
+-- Criação da tabela produtos
+CREATE TABLE IF NOT EXISTS produtos
+( codigo_produto INT(6) PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(60) NOT NULL,
+  unidade TEXT(10) NOT NULL,
+  ncm DECIMAL(12),
+  data_cadastro TIMESTAMP,
+  validade DATE,
+  lote VARCHAR(10)
+);
+
+-- Criação da tabela categorias
+CREATE TABLE IF NOT EXISTS categorias
+( id_categoria INT(4) PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(100) NOT NULL,
+  grupo VARCHAR(30),
+  data_cadastro TIMESTAMP
+);
+
+
+-- Criação da tabela pedidos (Chaves estrangeiras)
+CREATE TABLE IF NOT EXISTS pedidos
+( id_pedido INT(7) PRIMARY KEY AUTO_INCREMENT,
+  data_hora_pedido TIMESTAMP,
+  valor_pedido DECIMAL(10,2) NOT NULL,
+  cliente_cpf DECIMAL(11),
+  cod_produto INT(6),
+  -- Chave estrangeira conectando pedidos com clientes
+  FOREIGN KEY (cliente_cpf) REFERENCES clientes (cpf_cliente),
+  -- Chave estrangeira conectando pedidos com produtos
+  FOREIGN KEY (cod_produto) REFERENCES produtos (codigo_produto)
+);
+
+-- Criação da tabela notafiscal (Chaves estrangeiras)
+CREATE TABLE IF NOT EXISTS notafiscal
+( num_nf INT(9) PRIMARY KEY AUTO_INCREMENT,
+  data_hora_emissao TIMESTAMP,
+  valor_nf DECIMAL (10,2),
+  num_pedido INT(7),
+  -- Conectar com os pedidos
+  FOREIGN KEY (num_pedido) REFERENCES pedidos(id_pedido)
+);
